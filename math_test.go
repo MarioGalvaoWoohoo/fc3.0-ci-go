3d2cc827c758d2658386c84e4b4a014b9697cda1
Mario George Galvao <mario.galvao.woohoo@gmail.com>
2022-03-21 18:26:10 -0300

    Primeiro commit

---
+++ math_test.go
@@ -0,0 +1,10 @@
+import "testing"
+
+func TestSoma(t *testing.T) {
+
+	total := Soma(15, 15)
+
+	if total != 30 {
+		t.errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
+	}
+}
